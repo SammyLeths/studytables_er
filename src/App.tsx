a879761a1456837b39346d856cce6d2d33bec88c
SammyLeths <s.o.eyiowuawi@gmail.com>
2024-07-21 06:56:22 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { studyOneData } from "./data";
+import TableRow from "./TableRow";
+
+import "./App.css";
+
+const headers = [
+  "Symbol",
+  "Name",
+  "Price (Intraday)",
+  "Change",
+  "% Change",
+  "Volume",
+  "Avg Vol (3 months)",
+  "Market Cap",
+  "PE RTatio (TTM)",
+  "52 Week Range",
+];
+
+function App() {
+  return (
+    <div className="table_container pt-2">
+      <div className="title text-center pb-4 pt-2">
+        <h1 className="text-5xl">Smart Finance</h1>
+        <h3 className="text-base font-medium -mt-3">
+          Regular DataTable With Freeze Column
+        </h3>
+      </div>
+      <div className="p-3 bg-gray-300 overflow-hidden">
+        <div className="mx-auto overflow-x-auto">
+          <table className="min-w-full table-auto border border-slate-200">
+            <thead className="text-left">
+              <tr className="bg-sky-200 h-10">
+                <th className="px-3 border border-slate-200 sticky left-0 z-10 bg-sky-200">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox rounded border-slate-300"
+                    aria-label="Select All"
+                  />
+                </th>
+                {headers.map((data, index) => (
+                  <th
+                    key={index}
+                    className={`pl-3 pr-8 text-nowrap border border-slate-200 ${
+                      data == "Symbol"
+                        ? "sticky left-[41px] z-10 symbol-col bg-sky-200"
+                        : ""
+                    }`}
+                  >
+                    {data}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="text-left">
+              {studyOneData.map((data, index) => (
+                <TableRow data={data} key={data.symbol + index} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
